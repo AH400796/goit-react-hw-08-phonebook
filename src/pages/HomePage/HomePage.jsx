5a9fea824abb7da65324e253cf3e4cc5b273b6f1
AH400796 <A.Hokhman@gmail.com>
2023-03-03 19:08:38 +0200

    Update HomePage.jsx

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -1,10 +1,11 @@
 import { Title } from './HomePage.styled';
-import { showContactsList } from 'redux/contactsFormSlice';
+import { showContactsList, addContactsForm } from 'redux/contactsFormSlice';
 import { useDispatch } from 'react-redux';
 
 export default function HomePage() {
   const dispatch = useDispatch();
   dispatch(showContactsList(false));
+  dispatch(addContactsForm(false));
   return (
     <Title>An easy way to backup phone numbers of friends and colleagues</Title>
   );
